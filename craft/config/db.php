9009a245e58889157451a29f4911c3dc83e5d7c2
Winston Hearn <winston.hearn@voxmedia.com>
2017-04-16 14:39:41 -0400

    initial files from craft setup

--- craft/config/db.php
+++ craft/config/db.php
@@ -9,19 +9,21 @@
 
 return array(
 
-	// The database server name or IP address. Usually this is 'localhost' or '127.0.0.1'.
-	'server' => 'localhost',
-
-	// The name of the database to select.
-	'database' => '',
-
-	// The database username to connect with.
-	'user' => 'root',
-
-	// The database password to connect with.
-	'password' => '',
-
-	// The prefix to use when naming tables. This can be no more than 5 characters.
-	'tablePrefix' => 'craft',
+  '*' => array(
+      'tablePrefix' => 'craft',
+  ),
+  'localhost' => array(
+    'server' => 'localhost',
+    'user' => 'root',
+    'password' => 'root',
+    'database' => 'fdpa',
+    'port' => 8889,
+  ),
+  '.com' => array(
+      'server' => 'localhost',
+      'user' => 'root',
+      'password' => '',
+      'database' => '',
+  ),
 
 );
--- craft/config/general.php
+++ craft/config/general.php
@@ -8,26 +8,34 @@
  */
 
 return array(
+	'*' => array(
 
-	// Base site URL
-	'siteUrl' => null,
+		// Default Week Start Day (0 = Sunday, 1 = Monday...)
+		'defaultWeekStartDay' => 0,
+
+		// Enable CSRF Protection (recommended, will be enabled by default in Craft 3)
+		'enableCsrfProtection' => true,
+
+		// Whether "index.php" should be visible in URLs (true, false, "auto")
+		'omitScriptNameInUrls' => false,
+
+		// Control Panel trigger word
+		'cpTrigger' => 'admin',
+	),
+
+	'localhost' => array(
+		// Dev Mode (see https://craftcms.com/support/dev-mode)
+		'devMode' => true,
+	),
+
+	'fairdistrictspa.com' => array(
+		// Dev Mode (see https://craftcms.com/support/dev-mode)
+		'devMode' => false,
+	)
+
 
 	// Environment-specific variables (see https://craftcms.com/docs/multi-environment-configs#environment-specific-variables)
-	'environmentVariables' => array(),
+	// 'environmentVariables' => array(),
 
-	// Default Week Start Day (0 = Sunday, 1 = Monday...)
-	'defaultWeekStartDay' => 0,
-
-	// Enable CSRF Protection (recommended, will be enabled by default in Craft 3)
-	'enableCsrfProtection' => true,
-
-	// Whether "index.php" should be visible in URLs (true, false, "auto")
-	'omitScriptNameInUrls' => 'auto',
-
-	// Control Panel trigger word
-	'cpTrigger' => 'admin',
-
-	// Dev Mode (see https://craftcms.com/support/dev-mode)
-	'devMode' => false,
 
 );
