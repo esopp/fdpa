671ceaf6d18d7d7110bd15374e4f447ee089a0fd
Christopher Kennedy <chris@onebrightlight.com>
2017-04-19 17:00:22 -0700

    Added multi environment functionality using .env.php

--- craft/config/db.php
+++ craft/config/db.php
@@ -7,23 +7,27 @@
  * You can see a list of the default settings in craft/app/etc/config/defaults/db.php
  */
 
+// $_ENV constants are loaded by craft-multi-environment from .env.php via public/index.php
 return array(
 
-  '*' => array(
-      'tablePrefix' => 'craft',
-  ),
-  'localhost' => array(
-    'server' => 'localhost',
-    'user' => 'root',
-    'password' => 'root',
-    'database' => 'fdpa',
-    'port' => 8889,
-  ),
-  '.com' => array(
-      'server' => 'localhost',
-      'user' => 'root',
-      'password' => '',
-      'database' => '',
-  ),
+    // All environments
+    '*' => array(
+        'tablePrefix' => 'craft',
+        'server' => getenv('CRAFTENV_DB_HOST'),
+        'database' => getenv('CRAFTENV_DB_NAME'),
+        'user' => getenv('CRAFTENV_DB_USER'),
+        'password' => getenv('CRAFTENV_DB_PASS'),
+    ),
 
+    // Live (production) environment
+    'live'  => array(
+    ),
+
+    // Staging (pre-production) environment
+    'staging'  => array(
+    ),
+
+    // Local (development) environment
+    'local'  => array(
+    ),
 );
